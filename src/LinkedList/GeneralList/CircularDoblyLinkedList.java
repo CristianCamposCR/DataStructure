9fb02f49f2cb8a8d3522c28b5f816ac69d7efd1b
Cristian CR <20213tn003@utez.edu.mx>
2022-12-10 16:38:28 -0600

    Lista doble circular de comidas
    Se agrego la clase lista doble cirular y se modifico la clase de meal

---
+++ src/LinkedList/GeneralList/CircularDoblyLinkedList.java
@@ -0,0 +1,71 @@
+package LinkedList.GeneralList;
+
+import Interfaces.ILinkedList;
+import LinkedList.GeneralNodes.DoublyNode;
+import LinkedList.GeneralNodes.Node;
+
+public abstract class CircularDoblyLinkedList<T> implements ILinkedList<T> {
+
+    DoublyNode<T> head;
+
+    public CircularDoblyLinkedList() {
+        this.head = null;
+    }
+
+    public CircularDoblyLinkedList(DoublyNode<T> head) {
+        this.head = head;
+    }
+
+    public Node<T> getHead(){
+        return head;
+    }
+
+    public void setHead(DoublyNode<T> head) {
+        this.head = head;
+    }
+
+
+    //metodo para insertar en la lista
+    @Override
+    public void insert(T data) {
+        DoublyNode<T> newNodo = new DoublyNode<>(data);
+
+        if (this.head == null){
+            this.head = newNodo;
+            this.head.setNext(this.head);
+            this.head.setPrev(this.head);
+        }else{
+            DoublyNode<T> aux = this.head;
+            while (aux.getNext()!=this.head){
+                aux = (DoublyNode<T>) aux.getNext();
+            }
+            aux.setNext(newNodo);
+            newNodo.setPrev(aux);
+
+            this.head.setPrev(newNodo);
+            newNodo.setNext(this.head);
+        }
+    }
+
+    //metodo para quitar el ultimo elemento de la lista
+    @Override
+    public void remove(T data) {
+        DoublyNode<T> aux = this.head;
+
+        while (aux.getNext() != this.head){
+            aux = (DoublyNode<T>) aux.getNext();
+        }
+        this.head = (DoublyNode<T>) this.head.getNext();
+        this.head.setPrev(aux);
+        aux.setNext(this.head);
+    }
+
+
+
+
+    //metodos abstractos a implementar en otras clases
+    public abstract void searchByValue(T data);
+
+    public abstract void printList();
+
+}
--- src/model/Meal.java
+++ src/model/Meal.java
@@ -2,8 +2,27 @@ package model;
 
 public class Meal extends Product {
 
-    public Meal(String name, float price, String description) {
+
+    private String complements;
+    public Meal(String name, float price, String description,String complements) {
         super(name, price, description);
+        this.complements = complements;
+    }
+
+    public String getComplements() {
+        return complements;
+    }
+
+    public void setComplements(String complements) {
+        this.complements = complements;
+    }
+
+    @Override
+    public String toString() {
+        String text ="";
+        text = "Name: " + getName()+ "\tprice: " + getPrice() + "\tDescription: " + getDescription()
+                + "\tComplements: " + complements;
+        return text;
     }
 
     @Override
