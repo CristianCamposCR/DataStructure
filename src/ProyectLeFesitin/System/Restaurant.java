db5976b8c29a3259168f043f80adc3ea5b112ec0
Cristian CR <20213tn003@utez.edu.mx>
2022-12-11 21:27:34 -0600

    Menu recursivo

--- src/ProyectLeFesitin/System/Restaurant.java
+++ src/ProyectLeFesitin/System/Restaurant.java
@@ -4,8 +4,12 @@ import ProyectLeFesitin.Lists.DrinksCircularLinkedList;
 import ProyectLeFesitin.Lists.MealDoblyCricularLinkedList;
 import ProyectLeFesitin.Lists.TableLinkedListSimple;
 
+import java.util.Scanner;
+
 public class Restaurant {
     public static void main(String[] args) {
+        Scanner escanner = new Scanner(System.in);
+        int empelado;
         TableLinkedListSimple tables = new TableLinkedListSimple();
         DrinksCircularLinkedList listDrinks = new DrinksCircularLinkedList();
         MealDoblyCricularLinkedList listMeals = new MealDoblyCricularLinkedList();
@@ -15,6 +19,32 @@ public class Restaurant {
         listDrinks.printList();
         System.out.println("Meals");
         listMeals.printList();
+        switch (menu()) {
+            case 1:
+                System.out.println("si jalo");
+                break;
+            default:
+                System.out.println("Invalido");
+                break;
+        }
 
+    }
+
+
+    public static int menu() {
+        Scanner escanner = new Scanner(System.in);
+        int empleado = -1;
+        System.out.println("Bienvenido");
+        System.out.println("Lsita de empleados");
+        //listemployees.printList
+
+        System.out.println("Selecciona el empleado que eres");
+        try {
+            empleado = escanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Recuerda que solo debes ingresar el numero que le corresponde");
+            return menu();
+        }
+        return empleado;
     }
 }
